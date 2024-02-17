2c74202206e68e5c953ee45adbbbe207a3eb76e1
Peter Zhigalov <peter.zhigalov@gmail.com>
2024-02-17 19:45:38 +0700

    Update enchant

--- build_sylpheed_msys2.sh
+++ build_sylpheed_msys2.sh
@@ -107,7 +107,7 @@ includedir=\${prefix}/include
 
 Name: libenchant
 Description: A spell checking library
-Version: 2.6.5
+Version: 2.6.7
 Libs: -L\${libdir} -lenchant-2
 Cflags: -I\${includedir}/enchant-2
 EOF
